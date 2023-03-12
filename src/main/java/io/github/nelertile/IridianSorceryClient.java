0f3082e4afe2b8817be8e7a4c4ea97a1d0df5bbf
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2023-03-12 14:43:12 +0100

    init

---
+++ src/main/java/io/github/nelertile/IridianSorceryClient.java
@@ -0,0 +1,12 @@
+package io.github.nelertile;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IridianSorceryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
